2bb8b93184bbdde91863988daae8d0e5d699a67c
vongochieu7111999 <vongochieu2013@gmail.com>
2020-06-18 17:51:27 -0700

    Retrieve workout info in database and display in history fragment

--- app/src/main/java/com/example/myworkoutapplication/HistoryFragment.java
+++ app/src/main/java/com/example/myworkoutapplication/HistoryFragment.java
@@ -1,17 +1,69 @@
 package com.example.myworkoutapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HistoryFragment extends Fragment {
+  private FirebaseFirestore db;
+  private CollectionReference userHistory;
+  private TextView textViewData;
+  private User currentUser;
+  private static String DATE = "date";
+
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_history, container, false);
+    View root = inflater.inflate(R.layout.fragment_history, container, false);
+    textViewData = root.findViewById(R.id.text_view_data);
+    setData();
+    userHistory
+      .orderBy(DATE, Query.Direction.DESCENDING)
+      .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+      @Override
+      public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+        String data = "";
+        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+          UserHistory userHistory = documentSnapshot.toObject(UserHistory.class);
+          String workoutType = userHistory.getWorkoutType();
+          Date date = userHistory.getDate();
+          String time = userHistory.getTime();
+          DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+          String strDate = dateFormat.format(date);
+          data += "The workout is: " + workoutType;
+          data += "\nThe date is: " + strDate + "\nThe time workout is: " + time;
+          data += "\n\n";
+        }
+        textViewData.setText(data);
+      }
+    });
+
+    return root;
+  }
+
+  public void setData() {
+    db = FirebaseFirestore.getInstance();
+    currentUser = MainActivity.getCurrentUser();
+    String historyName = "history" + "-" + currentUser.getEmail();
+    userHistory = db.collection(historyName);
+
   }
 }
--- app/src/main/java/com/example/myworkoutapplication/StartWorkoutFragment.java
+++ app/src/main/java/com/example/myworkoutapplication/StartWorkoutFragment.java
@@ -7,28 +7,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.Date;
 
 public class StartWorkoutFragment extends Fragment {
   private Chronometer chronometer;
   private Button startButton;
   private Button pauseButton;
   private Button resetButton;
+  private EditText workOutTypeText;
   private long pauseOffSet;
   private boolean running;
+  private FirebaseFirestore db = FirebaseFirestore.getInstance();
+  private User currentUser = MainActivity.getCurrentUser();
 
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_startworkout, container, false);
-    chronometer = root.findViewById(R.id.chronometer);
-    startButton = root.findViewById(R.id.startwoButton);
-    pauseButton = root.findViewById(R.id.pausewoButton);
-    resetButton = root.findViewById(R.id.resetButton);
-    chronometer.setFormat("Time: %s");
-    chronometer.setBase(SystemClock.elapsedRealtime());
+    setData(root);
     startButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
@@ -53,12 +55,34 @@ public class StartWorkoutFragment extends Fragment {
       @Override
       public void onClick(View v) {
         if (!running) {
+          String workoutType = workOutTypeText.getEditableText().toString().trim();
+          String chroText = chronometer.getText().toString().substring(6);
+          Date date = new Date();
+          Toast.makeText(getContext(), "At " + date + "\nYou did a " + workoutType + " workout in: " + chroText, Toast.LENGTH_LONG).show();
+          setUserHistory(date, chroText, workoutType);
           chronometer.setBase(SystemClock.elapsedRealtime());
           pauseOffSet = 0;
         }
       }
     });
     return root;
+  }
+
+  public void setData(View root) {
+    chronometer = root.findViewById(R.id.chronometer);
+    startButton = root.findViewById(R.id.startwoButton);
+    pauseButton = root.findViewById(R.id.pausewoButton);
+    resetButton = root.findViewById(R.id.resetButton);
+    workOutTypeText = root.findViewById(R.id.workOutType);
+    chronometer.setFormat("Time: %s");
+    chronometer.setBase(SystemClock.elapsedRealtime());
+  }
+
+  public void setUserHistory(Date date, String time, String workoutType) {
+    UserHistory currentUserHistory = new UserHistory(date, time, workoutType);
+    currentUser = MainActivity.getCurrentUser();
+    String historyName = "history" + "-" + currentUser.getEmail();
+    db.collection(historyName).add(currentUserHistory);
   }
 }
 
